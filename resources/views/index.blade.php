9fe9a708ee93ef6bc5bdadfbcaa293f1a1efb81c
Ilham Setiaji <ilham.ahmadz18@gmail.com>
2022-08-10 15:49:00 +0700

    Added all article view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,8 @@
 @extends('components.template')
 @section('title','Desa Darungan Home')
 @section('hero')
-<section id="hero" class="hero d-flex align-items-center">
-    <div class="container-fluid">
+{{-- <section id="hero" class="hero d-flex align-items-center"> --}}
+    {{-- <div class="container-fluid">
         <br><br>
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
@@ -10,7 +10,7 @@
                     aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
                     aria-label="Slide 2"></button>
-                {{-- <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button> --}}
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
@@ -19,9 +19,9 @@
                 <div class="carousel-item">
                     <img src="assets/img/back.png" class="d-block w-100" alt="...">
                 </div>
-                {{-- <div class="carousel-item">
+                <div class="carousel-item">
                     <img src="assets/img/farmroad.png" class="d-block w-100" alt="...">
-                </div> --}}
+                </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
                 data-bs-slide="prev">
@@ -34,8 +34,41 @@
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
+    </div> --}}
+{{-- </section> --}}
+{{-- <div class="container-fluid"> --}}
+    {{-- <br><br> --}}
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            {{-- <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button> --}}
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="assets/img/front.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="assets/img/back.png" class="d-block w-100" alt="...">
+            </div>
+            {{-- <div class="carousel-item">
+                <img src="assets/img/farmroad.png" class="d-block w-100" alt="...">
+            </div> --}}
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-</section>
+{{-- </div> --}}
 <!-- End Hero Section -->
 @endsection
 @section('main-content')
